68e875a85004e5ead7a9bac8b04d6016fdca56c0
Macohi <sinconsistencia@gmail.com>
2026-02-13 23:27:39 -0600

    bump

--- minecraft/src/net/macohi/Version.java
+++ minecraft/src/net/macohi/Version.java
@@ -2,7 +2,7 @@ package net.macohi;
 
 public class Version {
 	public static String title = "Macohi Extension";
-	public static String version = "0.0";
+	public static String version = "1.0";
 
 	public static String fullversion()
 	{
